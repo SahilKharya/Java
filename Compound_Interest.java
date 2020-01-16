7258d32c4bc4ebe842b5b274f6fcf5909b5541f7
Sahil Kharya <kharyasahil@gmail.com>
2020-01-16 16:36:27 +0530

    updation

--- Compound_Interest.java
+++ Compound_Interest.java
@@ -1,22 +1,20 @@
-
 import java.util.Scanner;
 public class Compound_Interest {
 	public static void main(String[] args) {
 		System.out.println("\t Calculate Compound Interest \n\n");
-		Scanner sc = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter the Principle Value (P) to be Compounded : ");
-		double P = sc.nextInt();
+		double value = scan.nextDouble();
 		System.out.print("Enter the Rate of Interest (r) : ");
-		double r = sc.nextFloat();
+		double rate = scan.nextDouble();
 		System.out.print("Enter the time duration(t) in years : ");
-		int t = sc.nextInt();
+		int years = scan.nextInt();
 		System.out.print("Enter the number of times value is compounded per year :");
-		int n = sc.nextInt();
-		sc.close();
+		int periods = scan.nextInt();
+		scan.close();
 		
-		int time = n * t;  // total time of interest compounded
-		//As per the formula, Compound Interest = P(1+(r/(100*n)))^time
-		double CI = P * Math.pow(1+(r/(100*n)), time) - P;
-		System.out.println("The compound interest is : " + CI);
+		int times = periods * years;  // total time of interest compounded
+		double compoundInterest = value * Math.pow(1+(rate/(100*periods)), times) - value;
+		System.out.println("The compound interest is : " + compoundInterest);
 	}
 }

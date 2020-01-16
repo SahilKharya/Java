831eef870e1b1369637e38faa6fcc6599e59b3d8
Sahil Kharya <kharyasahil@gmail.com>
2020-01-16 16:41:45 +0530

    add

--- Vowel_Consonant.java
+++ Vowel_Consonant.java
@@ -4,11 +4,11 @@ public class Vowel_Consonant {
 
 	public static void main(String[] args) {
 		boolean vowel = false;
-		Scanner sc = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the character to check vowel or consonant : ");
-		char input = sc.next().charAt(0);
+		char input = scan.next().charAt(0);
 		char vowels[] = {'a','e','i','o','u','A','E','I','O','U'};
-		sc.close();
+		scan.close();
 		
 		for(int i=0; i<10; i++) {
 			if(input == vowels[i])

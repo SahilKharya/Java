337e87b61c5611c611b7a2f04365ec5654d2acf0
“SahilKharya” <kharyasahil@gmail.com>
2020-01-16 16:03:46 +0530

    updation

--- Sort_Strings.java
+++ Sort_Strings.java
@@ -1,32 +1,29 @@
 import java.util.Scanner;
-
 public class Sort_Strings {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Number of Strings : ");
-		int n = sc.nextInt();
-		Scanner sc2 = new Scanner(System.in);
-
-		String str[] = new String[n];
-		System.out.println("Strings :");
-
-		for(int i=0; i<n;i++) {
-			str[i] = sc2.nextLine();
-		}
-		String temp;
-		for(int i =0;i<n;i++) {
-			for(int j=0; j<n; j++) {
-				if(str[i].compareTo(str[j])<0) {
-					temp = str[i];
-					str[i] = str[j];
-					str[j] = temp;
-				}
-			}
-		}
-		System.out.println("Sorted Strings :");
-		for(int i=0; i<n;i++) {
-		System.out.println(str[i]+", ");
-		}
-		
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Number of Strings to sort: ");
+	        int count = sc.nextInt();
+	        Scanner scan2 = new Scanner(System.in);
+	
+	        String stringArray[] = new String[count];
+	        System.out.println("Strings :");
+	        for(int i=0; i<count;i++) {
+	                stringArray[i] = scan2.nextLine();
+	        }
+	        String temporarySorting;
+	        for(int i =0;i<count;i++) {
+	                for(int j=0; j<count; j++) {
+	                        if(stringArray[i].compareTo(stringArray[j])<0) {
+	                                temporarySorting = stringArray[i];
+	                                stringArray[i] = stringArray[j];
+	                                stringArray[j] = temporarySorting;
+	                        }
+	                }
+	        }
+	        System.out.println("Sorted Strings :");
+	        for(int i=0; i<count;i++) {
+	        	System.out.println(stringArray[i]+", ");
+	        }
 	}
 }

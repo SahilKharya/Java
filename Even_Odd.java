8e2554bd7339e11f1648b4192838be0eaf467c36
Sahil Kharya <kharyasahil@gmail.com>
2020-01-16 17:16:48 +0530

    add

--- Even_Odd.java
+++ Even_Odd.java
@@ -3,15 +3,13 @@
 import java.util.Scanner;
 public class Even_Odd {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the number : ");
-		int n = sc.nextInt();
+		int n = scan.nextInt();
 		sc.close();
 		if(n%2==0)
 			System.out.println("Given Number is even");
 		else
-			System.out.println("Given number is odd");
-		
-		
+			System.out.println("Given number is odd");	
 	}
 }

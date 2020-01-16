c06e68d3f1e397dd88a2a997a8c058a25c9d1099
Sahil Kharya <kharyasahil@gmail.com>
2020-01-16 17:04:17 +0530

    updation

--- Duplicate_Char.java
+++ Duplicate_Char.java
@@ -4,25 +4,18 @@ public class Duplicate_Char {
 	public static void main(String[] args) {
 		System.out.println("Find duplicate characters in a String \n");
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter String : ");
-		String text = sc.nextLine();
-		//System.out.println(text);
-		
-		char[] dups = new char[100];
-		int i, j, count = 0;
+		String text = scan.nextLine();		
 		
 		System.out.println("The duplicate characters are : ");
-		for(i=0; i<text.length(); i++) {
-			for(j=i+1; j<text.length(); j++) {
+		for(int i=0; i<text.length(); i++) {
+			for(int j=i+1; j<text.length(); j++) {
 				if(text.charAt(i) == text.charAt(j)) {
 					System.out.print(text.charAt(j) +", ");					
-					//dups[count] = text.charAt(j);					
-					count++;
 					break;
 				}				
 			}
 		}
-		//System.out.println(count);
 	}
 }

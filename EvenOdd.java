eb698297dda34fe496efd04be882614829336fb5
Sahil Kharya <kharyasahil@gmail.com>
2020-01-21 11:47:24 +0530

    Update and rename Even_Odd.java to EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,7 +1,7 @@
 
 
 import java.util.Scanner;
-public class Even_Odd {
+public class EvenOdd {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the number : ");
